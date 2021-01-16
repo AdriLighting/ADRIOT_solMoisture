c08735afcbf6e3472846498cdcc5044a38b9b27b
Grellard Adrien <grelard.adrien@gmail.com>
2021-01-16 15:37:16 +0100

    upd

--- examples/adriot_solMoisure_esp8266/adriot_solMoisure_esp8266.ino
+++ examples/adriot_solMoisure_esp8266/adriot_solMoisure_esp8266.ino
@@ -3,25 +3,20 @@
 #include <ADRIOT_relay.h>
 
 solmoistureClass * sensorSolMoisture;
-relaymanagement relay_managment;
 
-int		relayPump 				= -1;
-boolean relayPump_statu 		= false;
 boolean sensorSolMoisture_cap 	= false;
 
 void setup()
 {
 	sensorSolMoisture = new solmoistureClass(false,D1);
-	relay_managment.create(D2, relayPump);
 }
 
 void loop()
 {
-	relayPump_statu 		= relay_managment.relay(relayPump)->getStatus();	
 	sensorSolMoisture->loop(sensorSolMoisture_cap);
 	if (sensorSolMoisture_cap) {
-		if (!relayPump_statu) 	relay_managment.relay(relayPump)->open();
+		/* -- NEED WATER 		*/
 	} else {
-		if (relayPump_statu) 	relay_managment.relay(relayPump)->close();
+		/* -- NO NEED WATER 	*/
 	}
 }
